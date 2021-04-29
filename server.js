e672738d410615cff5cd1a487cb40763b7cbc4c6
Jeremy Seckinger <jtseck@aol.com>
2021-04-29 16:20:52 -0700

    merging

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //installed mongoose -J
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 2121
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -12,7 +12,26 @@ const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('.routes/main')
+const authRoutes = require('./routes/auth')
 
+require('dotenv').config({path: './config/.env'})
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public')) 
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json())
+
+app.use(
+    session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+)
 
 app.use('/', mainRoutes)
 
